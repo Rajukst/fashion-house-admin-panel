5a5991f8fd0016afb4c2b0a1423f60e478bf0463
Rajukst <rajuahamedkst15@gmail.com>
2022-10-17 09:59:54 +0600

    done

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,16 +1,15 @@
-
 import React from "react";
 import ProductChart from "../../components/AllCharts/ProductCharts/ProductChart";
 import Heading from "../../components/Heading-dashboard/Heading";
 import RecentOrder from "../../components/TopOrders/RecentOrder";
-
+import CompareSales from "../../components/CompareSales/CompareSales";
 const Dashboard = () => {
   return (
     <>
-     <Heading></Heading>
-     <ProductChart></ProductChart>
-     <RecentOrder></RecentOrder>
-     <CompareSales
+      <Heading></Heading>
+      <ProductChart></ProductChart>
+      <RecentOrder></RecentOrder>
+      <CompareSales></CompareSales>
     </>
   );
 };
