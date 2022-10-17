31043f7ad0d2f987052a0d8df4739ad08d1a9f22
Rajukst <rajuahamedkst15@gmail.com>
2022-10-17 16:08:29 +0600

    done

--- src/components/CompareSales/CompareSales.js
+++ src/components/CompareSales/CompareSales.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+import React from "react";
+import ReactApexChart from "react-apexcharts";
 
 const CompareSales = () => {
-   const series= [{
-        name: 'series1',
-        data: [31, 40, 28, 51, 42, 109, 100]
-      }, {
-        name: 'series2',
-        data: [11, 32, 45, 32, 34, 52, 41]
-      }]
-      const options= {
-        chart: {
-          height: 350,
-          type: 'area'
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'smooth'
-        },
-        xaxis: {
-          type: 'datetime',
-          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
-        },
-        tooltip: {
-          x: {
-            format: 'dd/MM/yy HH:mm'
-          },
-        },
-      }
-    return (
-        <div>
-              <ReactApexChart options={options} series={series} type="area" height={350} />
-        </div>
-    );
+  const series= [{
+    name: "Total Sales",
+    data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+}]
+const options= {
+  chart: {
+    height: 350,
+    type: 'line',
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  title: {
+    text: 'Product Sales by Month',
+    align: 'left'
+  },
+  grid: {
+    row: {
+      colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+      opacity: 0.5
+    },
+  },
+  xaxis: {
+    categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+  }
+}
+  return (
+    <div className="heading-chart">
+      <ReactApexChart options={options} series={series} type="line" height={350} />
+    </div>
+  );
 };
 
 export default CompareSales;
--- src/pages/AdProduct/AddProduct.js
+++ src/pages/AdProduct/AddProduct.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
 
 const AddProduct = () => {
-    return (
-        <div>
-            <h1>This is Add Product</h1>
-        </div>
-    );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [image, setImage] = useState(null);
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (!image) {
+      alert("no image inserted");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("image", image);
+    fetch("http://localhost:5000/pictures", {
+      method: "POST", // or 'PUT'
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if(data.insertedId){
+          alert("image upload success");
+          e.target.reset();
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+      
+  };
+  return (
+    <div>
+      <h1>This is Add Product</h1>
+      <form onSubmit={handleOnSubmit}>
+        <TextField
+          label="Name"
+          variant="standard"
+          required
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br /> <br />
+        <TextField
+          label="Email"
+          variant="standard"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <Button variant="contained" component="label">
+          Upload Photo
+          <input hidden accept="image/*" multiple type="file" onChange={e=>setImage(e.target.files[0])} />
+        </Button>
+        <br />
+        <br />
+        <p>Please upload valid jpg, png pictures</p>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      
+    </div>
+  );
 };
 
 export default AddProduct;
